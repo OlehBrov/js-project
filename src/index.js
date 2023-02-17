a4d052e09fbaf9e57867a10ec341bd1bab471616
OlehBrov <o.dudnyk85@gmail.com>
2023-02-17 12:05:18 +0200

    checkbox wrap

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const searchSlider = document.querySelector('.search-input__slider');
 const burgerMenu = document.querySelector('.header__menu-btn');
 const mobileMenuWrap = document.querySelector('.mobile-menu__wrap');
 const closeMobileMenu = document.querySelector('.mob__menu-close');
+const themeSelector = document.querySelector('#theme-selector');
+const themeSlider = document.querySelector('.theme-slider');
 
 searchBtn.addEventListener('click', onSearchBtn);
 closeMobileMenu.addEventListener('click', onCloseMobileMenu);
+themeSelector.addEventListener('change', onThemeChange)
 
 function onSearchBtn(e) {
   searchSlider.classList.add('is-visible');
@@ -28,4 +31,10 @@ function onCloseMobileMenu(e) {
   searchBtn.classList.remove('is-hidden');
   burgerMenu.classList.remove('is-hidden');
   closeMobileMenu.classList.add('is-hidden');
+}
+
+function onThemeChange(e) {
+  if (themeSelector.checked) {
+    themeSlider.classList.add('theme-slider-checked')
+  } else { themeSlider.classList.remove('theme-slider-checked') }
 }
