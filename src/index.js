f65e5c463632b54f6ce2ceefd55d0aa73c7cf667
OlehBrov <o.dudnyk85@gmail.com>
2023-02-26 18:56:22 +0200

    ttte

--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
-const searchWrap = document.querySelector('.search__wrap');
-const searchBtn = document.querySelector('.header__search-btn');
-const searchInput = document.querySelector('#news__search');
-const searchSlider = document.querySelector('.search-input__slider');
-const burgerMenu = document.querySelector('.header__menu-btn');
-const mobileMenuWrap = document.querySelector('.mobile-menu__wrap');
-const closeMobileMenu = document.querySelector('.mob__menu-close');
-const themeSelector = document.querySelector('#theme-selector');
-const themeSlider = document.querySelector('.theme-slider');
+import onResize from './js/resize';
+import { mqHandler } from './js/functions/mqHandler';
+import getCatagories from './js/filter';
+import './js/js-header/dark-mode';
+import './js/js-header/mobile-menu';
+import './js/js-read/read';
+import './js/localStorageFavorite';
+import { toLS } from './js/functions/saveToLocalStorage';
+// import './js/functions/eventLiCard';
+// import { calendar } from './js/calendar';
 
-searchBtn.addEventListener('click', onSearchBtn);
-closeMobileMenu.addEventListener('click', onCloseMobileMenu);
-themeSelector.addEventListener('change', onThemeChange)
+import { refs } from './js/refs';
 
-function onSearchBtn(e) {
-  searchSlider.classList.add('is-visible');
-  searchBtn.classList.add('is-hidden');
+if (
+  window.location.pathname === '/favorite.html' ||
+  window.location.pathname === '/read.html'
+) {
+  window.addEventListener('DOMContentLoaded', event => mqHandler());
 }
 
-burgerMenu.addEventListener('click', openMobileMenu);
-
-function openMobileMenu(e) {
-  mobileMenuWrap.classList.add('mob__menu-open');
-  searchBtn.classList.add('is-hidden');
-  burgerMenu.classList.add('is-hidden');
-  closeMobileMenu.classList.remove('is-hidden');
+console.log(window.location);
+if (
+  window.location.pathname === '/' ||
+  window.location.pathname === '/index.html'
+) {
+  refs.galleryList.addEventListener('click', toLS);
 }
 
-function onCloseMobileMenu(e) {
-  mobileMenuWrap.classList.remove('mob__menu-open');
-  searchBtn.classList.remove('is-hidden');
-  burgerMenu.classList.remove('is-hidden');
-  closeMobileMenu.classList.add('is-hidden');
-}
-
-function onThemeChange(e) {
-  if (themeSelector.checked) {
-    themeSlider.classList.add('theme-slider-checked')
-  } else { themeSlider.classList.remove('theme-slider-checked') }
-}
+console.dir(document);
